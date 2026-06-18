7ac19fb8ddaedf2a9d06b0eb2ff9a1c94b22c246
Cristian Braco <braco1209crist@gmail.com>
2026-06-18 15:44:32 -0500

    Unninstall toastify

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,71 @@
 // src/App.jsx
-import { useEffect } from "react";
-import { Routes, Route } from "react-router-dom";
-import { useTranslation } from "react-i18next";
-import ScrollToTop from "./components/ScrollToTop/ScrollToTop";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import { useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+import ScrollToTop from './components/ScrollToTop/ScrollToTop';
 
 // --- Páginas Públicas ---
-import Home from "./pages/Home/Home";
-import AdnMood from "./pages/AdnMood/AdnMood";
-import MoodPrint from "./pages/MoodPrint/MoodPrint";
-import MoodMind from "./pages/MoodMind/MoodMind";
-import Contact from "./pages/Contact/Contact";
-import Careers from "./pages/Careers/Careers";
+import Home from './pages/Home/Home';
+import AdnMood from './pages/AdnMood/AdnMood';
+import MoodPrint from './pages/MoodPrint/MoodPrint';
+import MoodMind from './pages/MoodMind/MoodMind';
+import Contact from './pages/Contact/Contact';
+import Careers from './pages/Careers/Careers';
 
 const App = () => {
-	const { t } = useTranslation();
+  const { t } = useTranslation();
 
-	useEffect(() => {
-		const originalTitle = document.title;
+  useEffect(() => {
+    const originalTitle = document.title;
 
-		const handleVisibilityChange = () => {
-			if (document.hidden) {
-				document.title = t("tab.hidden");
-			} else {
-				document.title = t("tab.visible") || originalTitle;
-			}
-		};
+    const handleVisibilityChange = () => {
+      if (document.hidden) {
+        document.title = t('tab.hidden');
+      } else {
+        document.title = t('tab.visible') || originalTitle;
+      }
+    };
 
-		document.addEventListener("visibilitychange", handleVisibilityChange);
-		return () => {
-			document.removeEventListener("visibilitychange", handleVisibilityChange);
-		};
-	}, [t]);
+    document.addEventListener('visibilitychange', handleVisibilityChange);
+    return () => {
+      document.removeEventListener('visibilitychange', handleVisibilityChange);
+    };
+  }, [t]);
 
-	return (
-		<>
-			<ScrollToTop />
-			<Routes>
-				{/* =========================================
+  return (
+    <>
+      <ScrollToTop />
+      <Routes>
+        {/* =========================================
             RUTAS PÚBLICAS (ACCESIBLES PARA TODOS)
             ========================================= */}
-				<Route path='/' element={<Home />} />
-				<Route path='/adn-mood' element={<AdnMood />} />
-				<Route path='/mood-print' element={<MoodPrint />} />
-				<Route path='/mood-mind' element={<MoodMind />} />
-				<Route path='/contacto' element={<Contact />} />
-				<Route path='/trabaja_con_nosotros' element={<Careers />} />
-			</Routes>
-			<ToastContainer
-				position='top-right'
-				autoClose={4000}
-				hideProgressBar={false}
-				newestOnTop={true}
-				closeOnClick
-				pauseOnFocusLoss
-				draggable
-				pauseOnHover
-				theme='light'
-			/>
-		</>
-	);
+        <Route
+          path='/'
+          element={<Home />}
+        />
+        <Route
+          path='/adn-mood'
+          element={<AdnMood />}
+        />
+        <Route
+          path='/mood-print'
+          element={<MoodPrint />}
+        />
+        <Route
+          path='/mood-mind'
+          element={<MoodMind />}
+        />
+        <Route
+          path='/contacto'
+          element={<Contact />}
+        />
+        <Route
+          path='/trabaja_con_nosotros'
+          element={<Careers />}
+        />
+      </Routes>
+    </>
+  );
 };
 
 export default App;
