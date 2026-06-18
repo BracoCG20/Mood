7cca003cab564d8382dd0553ffd6c1c99bef5077
Cristian Braco <braco1209crist@gmail.com>
2026-06-17 21:25:44 -0500

    Update mood

--- src/App.jsx
+++ src/App.jsx
@@ -1,118 +1,73 @@
-//src/App.jsx
-import { useEffect } from 'react';
-import { Routes, Route } from 'react-router-dom';
-import { useTranslation } from 'react-i18next';
-import ScrollToTop from './components/ScrollToTop/ScrollToTop';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+// src/App.jsx
+import { useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+import ScrollToTop from "./components/ScrollToTop/ScrollToTop";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 // --- Páginas Públicas ---
-import Home from './pages/Home/Home';
-import AdnMood from './pages/AdnMood/AdnMood';
-import MoodPrint from './pages/MoodPrint/MoodPrint';
-import MoodMind from './pages/MoodMind/MoodMind'; // 🌟 Importamos la nueva página
-import Contact from './pages/Contact/Contact';
-import Careers from './pages/Careers/Careers';
-import JobDetail from './pages/Careers/JobDetail';
-import JobApplication from './pages/Careers/JobApplication';
-
-// --- Archivos del CMS ---
-import CmsLogin from './pages/Cms/CmsLogin';
-import CmsDashboard from './pages/Cms/CmsDashboard';
-import { AuthProvider } from './context/AuthContext';
-import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute';
+import Home from "./pages/Home/Home";
+import AdnMood from "./pages/AdnMood/AdnMood";
+import MoodPrint from "./pages/MoodPrint/MoodPrint";
+import MoodMind from "./pages/MoodMind/MoodMind";
+import Contact from "./pages/Contact/Contact";
+import Careers from "./pages/Careers/Careers";
+import JobDetail from "./pages/Careers/JobDetail";
+import JobApplication from "./pages/Careers/JobApplication";
 
 const App = () => {
-  const { t } = useTranslation();
+	const { t } = useTranslation();
 
-  useEffect(() => {
-    const originalTitle = document.title;
+	useEffect(() => {
+		const originalTitle = document.title;
 
-    const handleVisibilityChange = () => {
-      if (document.hidden) {
-        document.title = t('tab.hidden');
-      } else {
-        document.title = t('tab.visible') || originalTitle;
-      }
-    };
+		const handleVisibilityChange = () => {
+			if (document.hidden) {
+				document.title = t("tab.hidden");
+			} else {
+				document.title = t("tab.visible") || originalTitle;
+			}
+		};
 
-    document.addEventListener('visibilitychange', handleVisibilityChange);
-    return () => {
-      document.removeEventListener('visibilitychange', handleVisibilityChange);
-    };
-  }, [t]);
+		document.addEventListener("visibilitychange", handleVisibilityChange);
+		return () => {
+			document.removeEventListener("visibilitychange", handleVisibilityChange);
+		};
+	}, [t]);
 
-  return (
-    <>
-      <ScrollToTop />
-      <AuthProvider>
-        <Routes>
-          {/* =========================================
-              RUTAS PÚBLICAS (ACCESIBLES PARA TODOS)
-              ========================================= */}
-          <Route
-            path='/'
-            element={<Home />}
-          />
-          <Route
-            path='/adn-mood'
-            element={<AdnMood />}
-          />
-          <Route
-            path='/mood-print'
-            element={<MoodPrint />}
-          />
-          <Route
-            path='/mood-mind'
-            element={<MoodMind />}
-          />
-          <Route
-            path='/contacto'
-            element={<Contact />}
-          />
-          <Route
-            path='/trabaja_con_nosotros'
-            element={<Careers />}
-          />
-          <Route
-            path='/trabaja_con_nosotros/:jobId'
-            element={<JobDetail />}
-          />
-          <Route
-            path='/trabaja_con_nosotros/:jobId/postular'
-            element={<JobApplication />}
-          />
-
-          {/* =========================================
-              RUTAS DEL CMS (ÁREA DE ADMINISTRACIÓN)
-              ========================================= */}
-          <Route
-            path='/cms/login'
-            element={<CmsLogin />}
-          />
-          <Route
-            path='/cms/dashboard'
-            element={
-              <ProtectedRoute>
-                <CmsDashboard />
-              </ProtectedRoute>
-            }
-          />
-        </Routes>
-        <ToastContainer
-          position='top-right'
-          autoClose={4000}
-          hideProgressBar={false}
-          newestOnTop={true}
-          closeOnClick
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-          theme='light'
-        />
-      </AuthProvider>
-    </>
-  );
+	return (
+		<>
+			<ScrollToTop />
+			<Routes>
+				{/* =========================================
+            RUTAS PÚBLICAS (ACCESIBLES PARA TODOS)
+            ========================================= */}
+				<Route path='/' element={<Home />} />
+				<Route path='/adn-mood' element={<AdnMood />} />
+				<Route path='/mood-print' element={<MoodPrint />} />
+				<Route path='/mood-mind' element={<MoodMind />} />
+				<Route path='/contacto' element={<Contact />} />
+				<Route path='/trabaja_con_nosotros' element={<Careers />} />
+				<Route path='/trabaja_con_nosotros/:jobId' element={<JobDetail />} />
+				<Route
+					path='/trabaja_con_nosotros/:jobId/postular'
+					element={<JobApplication />}
+				/>
+			</Routes>
+			<ToastContainer
+				position='top-right'
+				autoClose={4000}
+				hideProgressBar={false}
+				newestOnTop={true}
+				closeOnClick
+				pauseOnFocusLoss
+				draggable
+				pauseOnHover
+				theme='light'
+			/>
+		</>
+	);
 };
 
 export default App;
